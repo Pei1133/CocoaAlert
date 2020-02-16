e9f0adb5faad43da40a5a2c2e9be755ae75dbf82
Pei1133 <pei.7969pei.7969@gmail.com>
2020-02-16 19:34:50 +0800

    Add AlertHelper.

---
+++ CocoaAlert/CocoaAlert/AlertHelper.swift
@@ -0,0 +1,16 @@
+//
+//  AlertHelper.swift
+//  CocoaAlert
+//
+//  Created by Peii on 2020/2/16.
+//  Copyright © 2020 Huang. All rights reserved.
+//
+
+import Foundation
+
+public class AlertHelper: NSObject {
+    
+    public func getAlertString() -> String {
+        return "Warning! Warning!"
+    }
+}
---
+++ CocoaAlert/CocoaAlert/CocoaAlert.h
@@ -0,0 +1,19 @@
+//
+//  CocoaAlert.h
+//  CocoaAlert
+//
+//  Created by Peii on 2020/2/16.
+//  Copyright © 2020 Huang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaAlert.
+FOUNDATION_EXPORT double CocoaAlertVersionNumber;
+
+//! Project version string for CocoaAlert.
+FOUNDATION_EXPORT const unsigned char CocoaAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaAlert/PublicHeader.h>
+
+
